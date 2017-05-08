489cfe1bbcf03e51d2070982966afd12ebcdfc1a
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-08 16:08:26 -0600

    render geolocation data (first pass)
    - change the App component to be stageful
    - set the default state to a trivial initial string
    - in componentDidMount, call the getCurrentPosition
    on the geo object
    - the success callback will set state to the position
    object fetched from the geolocation api
    - in render(), render the timestamp of the position
    object as a initial way to display some data

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import  { geo }  from '../data/geolocation';
 
-const App = () => {
-  return (
-    <div>Hey!</div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      position: "nothing"
+    }
+  }
+
+  componentDidMount() {
+    let that = this;
+    function error(err) {
+      that.setState({ position: err.message })
+    }
+
+    function success(position) {
+      that.setState({ position })
+    }
+
+    let options = {
+      timeout: 5000
+    }
+
+    return geo.getCurrentPosition(success, error, options);
+  }
+
+  render(){
+    return (
+      <div> Hello, { this.state.position.timestamp }</div>
+    )
+  }
 }
 
 export default App;
