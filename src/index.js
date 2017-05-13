e2e0eba6954dc74f2c8104a8bdf17f54515518b9
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-12 18:58:56 -0600

    begin hooking up redux 6: create store
    * in index.js, create the store

--- src/index.js
+++ src/index.js
@@ -6,9 +6,7 @@ import Routes from './routes';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
-const middleware = [thunk];
-
-const store = createStore(rootReducer, {}, applyMiddleware(...middleware))
+const store = createStore();
 
 render(
   <Provider store={store}>
