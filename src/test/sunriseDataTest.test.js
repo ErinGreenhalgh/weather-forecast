7957bc4a7a8cdc8be2672e0f74698efec97e3df9
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-15 14:38:15 -0600

    replace calls to fetchMock() with calls to moxios in test
    *  install maxis in dev dependencies
    * create the stubbed response
    * expectations about the data returned need to happen
    within the wait() function

--- src/test/sunriseDataTest.test.js
+++ src/test/sunriseDataTest.test.js
@@ -1,55 +1,37 @@
-import fetchMock from 'fetch-mock';
+import axios from 'axios';
+import moxios from 'moxios';
+
 import { sunriseUrl, getSunriseData }  from '../data/sunriseAndSunset';
 
 describe('sunrise data api', () => {
 
-  // afterEach( () => {
-  //   // expect(fetchMock.calls().unmatched).toEqual([]);
-  //   // fetchMock.restore();
-  //   fetchMock.reset();
-  // });
+  beforeEach( () => {
+    moxios.install();
+  });
+
+  afterEach( () => {
+    moxios.uninstall();
+  });
 
   it('takes in coordinates and date and returns data', () => {
-    let url = "https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=2017-05-11"
+    let data = getSunriseData();
 
-    const options = {
-      method: "GET",
-      response: {
-        results: {
-          sunrise: "7:27:02 AM",
-          sunset: "5:05:55 PM"
-        },
-      times: 3
+    const mockedResponse = { results:
+      {
+      sunrise: "7:27:02 AM",
+      sunset: "5:05:55 PM"
       }
     }
 
-    const response = {
-      results: {
-        sunrise: "7:27:02 AM",
-        sunset: "5:05:55 PM"
-      }
-    }
-
-    fetchMock.mock(url, options).catch();
-    // fetchMock.mock(url, options).spy();
-
-    const calls = fetchMock.calls(url);
-
-    const coords = {
-      latitude: 36.7201600,
-      longitude: -4.4203400
-    }
-    const date  = "2017-05-11"
-
-    const data = getSunriseData(sunriseUrl, coords, date);
-
-    console.log("calls:", calls);
-    expect(fetchMock.called(url)).toEqual(true);
-
-
-    // expect(fetchMock.lastUrl()).toEqual(url);
-    // expect(fetchMock.lastOptions()).toEqual("things")
-    // expect(data.results.sunrise).toEqual("7:27:02 AM");
-    // expect(data.results.sunset).toEqual("5:05:55 PM");
-  })
-})
+    moxios.wait( () => {
+      let request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: mockedResponse
+      }).then( () => {
+        expect(data.results.sunrise).toEqual(mockedResponse.results.sunrise);
+        expect(data.results.sunset).toEqual(mockedResponse.results.sunset);
+      });
+    });
+  });
+});
