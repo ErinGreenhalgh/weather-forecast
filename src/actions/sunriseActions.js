c25ce3e61ec07f2d1037b1a07cade0ad522153ce
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-16 10:35:57 -0600

    try to debug problem with chained api calls:
    * added console.log statements in sunriseActions to 
    trace problem: the coords async returns a promise that
    returns coordinates when resolved, but after calling
    the getSunriseData function, data is undefined

--- src/actions/sunriseActions.js
+++ src/actions/sunriseActions.js
@@ -3,16 +3,19 @@ import { coordsAsync } from '../data/geolocation';
 import { getSunriseData } from '../data/sunriseAndSunset';
 
 export const fetchSunriseDataSuccess = (data) => {
-  console.log('success!')
+  console.log('in success action:', data)
   return { type: types.FETCH_SUNRISE_DATA_SUCCESS, sunriseData: data }
 }
 export const fetchSunriseData = options => dispatch => {
   coordsAsync()
   .then(coords => {
+    console.log("coords:", coords)
     getSunriseData(coords, "today")
+    //this returns a promise so we should be able to chain it...
   })
-  .then(data => {
-    dispatch(fetchSunriseDataSuccess(data))
+  .then(response => {
+    console.log("sunriseData:", response)
+    dispatch(fetchSunriseDataSuccess(response))
    })
   .catch(error => {
     console.log("sunrise data error:", error)
