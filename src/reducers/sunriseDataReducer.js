74394b6236e928466e8c757f6a195e9cc168c142
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-05-16 16:14:18 -0600

    fix immutable-state-invariant-error coming from reducer:
    * in order for Object.assign to not mutate state, 
    the first argument should be an empty object. This
    argument will be mutated, whereas the properties of
    the other args will be copied into this object

--- src/reducers/sunriseDataReducer.js
+++ src/reducers/sunriseDataReducer.js
@@ -3,7 +3,7 @@ import * as types from '../actions/actionTypes';
 export const sunriseDataReducer = (state={}, action) => {
   switch(action.type){
     case types.FETCH_SUNRISE_DATA_SUCCESS:
-      return Object.assign(state, action.sunriseData);
+      return Object.assign({}, state, action.sunriseData);
       break;
     default:
       return state;
