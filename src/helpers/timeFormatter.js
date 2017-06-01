f1278e571a27ddae488f32672be24158fd5f972f
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-06-01 13:49:33 -0600

    create function for converting from ISO string to minutes
    * split the string, map all elements to ints, 
    then convert hours and seconds to minutes and
    add everything
    * return a rounded result

--- src/helpers/timeFormatter.js
+++ src/helpers/timeFormatter.js
@@ -3,4 +3,16 @@ const convertToLocalTime = (utcTime) => {
   //get the time in minutes from the utcTime
   //add the offset to the time in minutes
   //(will still work for negative offsets)
+export const convertIso8601ToMinutes = (isoDateTime) => {
+  var rawTime = isoDateTime.split("T")[1];
+  var timeElements = rawTime.split(":");
+  var times = timeElements.map( element => {
+    return parseInt(element);
+  })
+  const hours = times[0];
+  const minutes = times[1];
+  const seconds = times[2];
+
+  const totalMinutes = (hours * 60) + minutes + ( seconds / 60);
+  return Math.round(totalMinutes);
 }
