322b9603b6785cb73d8ed3f2d6e9754861b92fb3
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-06-01 13:50:49 -0600

    create function for converting minutes to an iso time string
    * this function contains math for converting hours
    to minutes and rules for string formatting
    * eventually these should be separated out

--- src/helpers/timeFormatter.js
+++ src/helpers/timeFormatter.js
@@ -3,6 +3,17 @@ const convertToLocalTime = (utcTime) => {
   //get the time in minutes from the utcTime
   //add the offset to the time in minutes
   //(will still work for negative offsets)
+
+export const convertMinutesToIso = (timeInMinutes) => {
+  const hours = Math.floor(timeInMinutes / 60);
+  var minutes = timeInMinutes % 60;
+  if (minutes == 0) {
+    minutes = "00"
+  }
+  //final formatting to string should be in a differnt method
+  return hours.toString() + ":" + minutes.toString();
+}
+
 export const convertIso8601ToMinutes = (isoDateTime) => {
   var rawTime = isoDateTime.split("T")[1];
   var timeElements = rawTime.split(":");
