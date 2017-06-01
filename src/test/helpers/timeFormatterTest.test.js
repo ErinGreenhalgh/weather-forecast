27cb96e09f7cb3b834fd0c693c94577c360f0e7b
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-06-01 16:45:08 -0600

    tweak test import and expected values
    * import the functions as attributes on a formatter object
    * change expected times in lines 47 and 54 to include
    the correctly appended zeros for military time

--- src/test/helpers/timeFormatterTest.test.js
+++ src/test/helpers/timeFormatterTest.test.js
@@ -1,12 +1,12 @@
-import { convertToLocalTime, convertMinutesToIso, convertIso8601ToMinutes } from '../../helpers/timeFormatter';
+import * as formatter from '../../helpers/timeFormatter';
 
 describe('time formatter', () => {
   it('converts an ISO 8601 datetime to time in minutes', () => {
     const isoTime1 = '2017-05-31T06:00:00';
-    expect(convertIso8601ToMinutes(isoTime1)).toEqual(360);
+    expect(formatter.convertIso8601ToMinutes(isoTime1)).toEqual(360);
 
     const isoTime2 = '2017-05-31T03:12:16';
-    expect(convertIso8601ToMinutes(isoTime2)).toEqual(192);
+    expect(formatter.convertIso8601ToMinutes(isoTime2)).toEqual(192);
   });
 
   it('correctly formats the display time', () => {
@@ -44,17 +44,17 @@ describe('time formatter', () => {
 
   it('converts time in minutes to an ISO 8601 time', () => {
     const minutes1 = 240;
-    expect(convertMinutesToIso(minutes1)).toEqual("4:00");
+    expect(formatter.convertMinutesToIso(minutes1)).toEqual("04:00");
 
     const minutes2 = 735;
-    expect(convertMinutesToIso(minutes2)).toEqual("12:15");
+    expect(formatter.convertMinutesToIso(minutes2)).toEqual("12:15");
   })
 
   it('converts time from UTC to local time', () => {
-    const expectedDenverTime = '6:00';
+    const expectedDenverTime = '06:00';
     const utcTime = '2017-05-31T12:00:00+00:00';
 
-    const result = convertToLocalTime(utcTime);
+    const result = formatter.convertToLocalTime(utcTime);
     expect(result).toEqual(expectedDenverTime);
   });
 });
