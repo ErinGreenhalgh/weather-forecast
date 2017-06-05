2acc5835f56d8ef33175d8e01e81e6239e912774
Erin Greenhalgh <erin.greenhalgh0113@gmail.com>
2017-06-05 11:32:13 -0600

    add temp conversion in weather success action:
    * upon fetchConditionsSuccess, convert the kelvin temp that comes in from the api to F
    * this will be displayed to the user

--- src/actions/weatherActions.js
+++ src/actions/weatherActions.js
@@ -1,12 +1,13 @@
 import * as types from './actionTypes';
 import { coordsAsync } from '../data/geolocation';
 import WeatherAPIMock from '../mockAPI/weatherConditionsMock';
+import * as tempConverter from '../helpers/temperatureConverter';
 
 export const fetchConditionsSuccess = data => {
   return {
     type: types.FETCH_CONDITIONS_SUCCESS,
     conditions: {
-      temperature: data.main.temp,
+      temperature: tempConverter.kelvinToFahrenheit(data.main.temp),
       description: data.weather[0].description,
     }
   }
